596acc42c495b65e9b6cf054bd9c92a6b5c53f40
Josh Rosen <joshrosen@databricks.com>
2014-12-10 13:29:05 -0800

    Use app version as cache key prefix.
    This prevents the live site’s warmup queries from
    clobbering the cache of the preview version and
    vice-versa.

--- sparkprs/__init__.py
+++ sparkprs/__init__.py
@@ -4,6 +4,7 @@ from flask.ext.cache import Cache
 
 app = Flask('sparkprs', static_folder="../static", template_folder="../templates")
 app.config.from_pyfile('../settings.cfg')
-cache = Cache(app, config={'CACHE_TYPE': 'memcached'})
 
 VERSION = os.environ['CURRENT_VERSION_ID']
+
+cache = Cache(app, config={'CACHE_TYPE': 'memcached', 'CACHE_KEY_PREFIX': VERSION})
