8916af5c2da4b721d60956fa06dd9c5d4f725c9c
Josh Rosen <joshrosen@databricks.com>
2015-08-14 14:55:04 -0700

    Modularization of PR update task.

--- sparkprs/controllers/tasks.py
+++ sparkprs/controllers/tasks.py
@@ -1,24 +1,29 @@
-import json
-from dateutil.parser import parse as parse_datetime
-from dateutil import tz
 from datetime import datetime
 import itertools
+import json
 import logging
 
-from flask import Blueprint
+from flask import Blueprint, url_for
 from google.appengine.api import taskqueue
 import feedparser
-
-from sparkprs import app
-from sparkprs.models import Issue, JIRAIssue, KVS
-from sparkprs.github_api import raw_github_request, ISSUES_BASE
 from link_header import parse as parse_link_header
+from dateutil.parser import parse as parse_datetime
+from dateutil import tz
+
+from sparkprs.models import Issue, JIRAIssue, KVS
+from sparkprs.github_api import raw_github_request, paginated_github_request, PULLS_BASE, \
+    ISSUES_BASE
+from sparkprs import app
+from sparkprs.jira_api import start_issue_progress, link_issue_to_pr
 
 
 tasks = Blueprint('tasks', __name__)
 
 
-@tasks.route("/update-github-prs")
+oauth_token = app.config['GITHUB_OAUTH_KEY']
+
+
+@tasks.route("/github/update-prs")
 def update_github_prs():
     def fetch_and_process(url):
         logging.debug("Following url %s" % url)
@@ -31,7 +36,7 @@ def update_github_prs():
                 parse_datetime(pr['updated_at']).astimezone(tz.tzutc()).replace(tzinfo=None)
             is_fresh = (now - updated_at).total_seconds() < app.config['FRESHNESS_THRESHOLD']
             queue_name = ("fresh-prs" if is_fresh else "old-prs")
-            taskqueue.add(url="/tasks/update-github-pr/%i" % pr['number'], queue_name=queue_name)
+            taskqueue.add(url=url_for(".update_pr", pr_number=pr['number']), queue_name=queue_name)
         for link in link_header.links:
             if link.rel == 'next':
                 fetch_and_process(link.href)
@@ -44,10 +49,85 @@ def update_github_prs():
     return "Done fetching updated GitHub issues"
 
 
-@tasks.route("/update-github-pr/<int:number>", methods=['GET', 'POST'])
-def update_pr(number):
-    Issue.get_or_create(number).update(app.config['GITHUB_OAUTH_KEY'])
-    return "Done updating pull request %i" % number
+@tasks.route("/github/update-pr/<int:pr_number>", methods=['GET', 'POST'])
+def update_pr(pr_number):
+    logging.debug("Updating pull request %i" % pr_number)
+    pr = Issue.get_or_create(pr_number)
+    issue_response = raw_github_request(PULLS_BASE + '/%i' % pr_number,
+                                        oauth_token=oauth_token, etag=pr.etag)
+    if issue_response is None:
+        logging.debug("PR %i hasn't changed since last visit; skipping" % pr_number)
+        return "Done updating pull request %i (nothing changed)" % pr_number
+    pr.pr_json = json.loads(issue_response.content)
+    pr.etag = issue_response.headers["ETag"]
+    pr.state = pr.pr_json['state']
+    pr.user = pr.pr_json['user']['login']
+    pr.updated_at = \
+        parse_datetime(pr.pr_json['updated_at']).astimezone(tz.tzutc()).replace(tzinfo=None)
+
+    for issue_number in pr.parsed_title['jiras']:
+        try:
+            link_issue_to_pr("SPARK-%s" % issue_number, pr)
+        except:
+            logging.exception("Exception when linking to JIRA issue SPARK-%s" % issue_number)
+        try:
+            start_issue_progress("SPARK-%s" % issue_number)
+        except:
+            logging.exception(
+                "Exception when starting progress on JIRA issue SPARK-%s" % issue_number)
+
+    pr.put()  # Write our modifications back to the database
+
+    subtasks = [".update_pr_comments", ".update_pr_review_comments", ".update_pr_files"]
+    for task in subtasks:
+        taskqueue.add(url=url_for(task, pr_number=pr_number), queue_name='fresh-prs')
+
+    return "Done updating pull request %i" % pr_number
+
+
+@tasks.route("/github/update-pr-comments/<int:pr_number>", methods=['GET', 'POST'])
+def update_pr_comments(pr_number):
+    pr = Issue.get(pr_number)
+    comments_response = paginated_github_request(ISSUES_BASE + '/%i/comments' % pr_number,
+                                                 oauth_token=oauth_token)
+    # TODO: after fixing #32, re-enable etags here: etag=self.comments_etag)
+    if comments_response is None:
+        return "Comments for PR %i are up-to-date" % pr_number
+    else:
+        pr.comments_json, pr.comments_etag = comments_response
+        pr.cached_commenters = pr._compute_commenters()
+        pr.cached_last_jenkins_outcome = None
+        pr.last_jenkins_outcome  # force recomputation of Jenkins outcome
+        pr.put()  # Write our modifications back to the database
+        return "Done updating comments for PR %i" % pr_number
+
+
+@tasks.route("/github/update-pr-review-comments/<int:pr_number>", methods=['GET', 'POST'])
+def update_pr_review_comments(pr_number):
+    pr = Issue.get(pr_number)
+    pr_comments_response = paginated_github_request(PULLS_BASE + '/%i/comments' % pr_number,
+                                                    oauth_token=oauth_token)
+    # TODO: after fixing #32, re-enable etags here: etag=self.pr_review_comments_etag)
+    if pr_comments_response is None:
+        return "Review comments for PR %i are up-to-date" % pr_number
+    else:
+        pr.pr_comments_json, pr.pr_comments_etag = pr_comments_response
+        pr.cached_commenters = pr._compute_commenters()
+        pr.put()  # Write our modifications back to the database
+        return "Done updating review comments for PR %i" % pr_number
+
+
+@tasks.route("/github/update-pr-files/<int:pr_number>", methods=['GET', 'POST'])
+def update_pr_files(pr_number):
+    pr = Issue.get(pr_number)
+    files_response = paginated_github_request(PULLS_BASE + "/%i/files" % pr_number,
+                                              oauth_token=oauth_token, etag=pr.files_etag)
+    if files_response is None:
+        return "Files for PR %i are up-to-date" % pr_number
+    else:
+        pr.files_json, pr.files_etag = files_response
+        pr.put()  # Write our modifications back to the database
+        return "Done updating files for PR %i" % pr_number
 
 
 @tasks.route("/update-jira-issues")
