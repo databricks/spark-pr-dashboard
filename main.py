6eea52e00a81c1efe012c0acda539b844801b9f7
Josh Rosen <joshrosen@databricks.com>
2018-09-03 00:05:45 -0700

    Use requests_toolbelt appengine adapter
    See https://stackoverflow.com/a/37304524

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from flask import render_template
+import requests
+import requests_toolbelt.adapters.appengine
 
 from sparkprs import app
 from sparkprs.controllers.tasks import tasks
@@ -6,6 +8,10 @@ from sparkprs.controllers.admin import admin
 from sparkprs.controllers.login import login
 from sparkprs.controllers.jenkins import jenkins
 from sparkprs.controllers.prs import prs
+
+# See the 'Requests' tab of
+# https://cloud.google.com/appengine/docs/standard/python/issue-requests#issuing_an_http_request
+requests_toolbelt.adapters.appengine.monkeypatch()
 
 app.register_blueprint(login)
 app.register_blueprint(jenkins)
